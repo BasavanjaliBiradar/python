55570802573f3f42a5705abf2c0c6e59864b3a2d
Basavanjali <basavanjalib44444@gmail.com>
2023-05-15 14:28:21 +0530

    Update python_removei'thelemnt_in string.py

--- python_removei'thelemnt_in string.py
+++ python_removei'thelemnt_in string.py
@@ -1,3 +1,10 @@
 str=input("please enter the string:")#anjali
 ch=input("please enter the character to be removed:")#a
 print(str.replace(ch,""))# nj li
+
+# remove i'th element from a string USING REPLACE METHOD
+def removech(s1,s2):
+    print(s1.replace(s2," "))
+s1=input("please give a string :")
+s2=input("please enter the character to be removed:")
+removech(s1,s2)
