d2c8ff6f4da45bfbf6fa85d6a86a215529c5f8f6
Basavanjali <basavanjalib44444@gmail.com>
2023-05-15 14:27:36 +0530

    Update python_substringinstring.py

--- python_substringinstring.py
+++ python_substringinstring.py
@@ -1,3 +1,6 @@
-str=input("please enter the string:")#anjali
-ch=input("please enter the character to be removed:")#a
-print(str.replace(ch,""))# nj li
+str=input("please enter the string:")#anjali vivek
+substr=input("please enter the substring to check in string:")#i
+if substr in str:
+    print(f"{substr} is present in string")# this print statement will be the output
+else:
+    print(f"{substr} is not present in string")
